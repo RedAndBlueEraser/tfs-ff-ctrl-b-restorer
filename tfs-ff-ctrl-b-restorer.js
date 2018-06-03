8e72c5f9e0c2b84523c7daf3aade54f7cc31f07e
RedAndBlueEraser <RedAndBlueEraser@users.noreply.github.com>
2018-06-03 10:52:19 +0000

    Changed inline comments to block comments
    Allows script to be easily copied into a bookmarklet

--- tfs-ff-ctrl-b-restorer.js
+++ tfs-ff-ctrl-b-restorer.js
@@ -1,6 +1,6 @@
 /*
  * tfs-ff-ctrl-b-restorer.js
- * Version 20180527
+ * Version 20180603
  * Written by Harry Wong (RedAndBlueEraser)
  */
 var f;
@@ -12,13 +12,13 @@ var f;
                 if (k === 98 || k === 105 || k === 117) {
                     e.preventDefault();
                     switch (k) {
-                        case 98:   // Ctrl+B
+                        case 98:   /* Ctrl+B */
                             $(this).find(".richeditor-toolbar-bold").click();
                             break;
-                        case 105:  // Ctrl+I
+                        case 105:  /* Ctrl+I */
                             $(this).find(".richeditor-toolbar-italic").click();
                             break;
-                        case 117:  // Ctrl+U
+                        case 117:  /* Ctrl+U */
                             $(this).find(".richeditor-toolbar-underline").click();
                             break;
                     }
